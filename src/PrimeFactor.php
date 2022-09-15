053f6f968875460f85661814ffed764b8cec579f
Alexotieno1717 <alexotieno900@gmail.com>
2022-09-15 12:16:03 +0300

    Prime Factor coding in php

---
+++ src/PrimeFactor.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App;
+
+
+class PrimeFactor
+{
+
+    public function generate($number)
+    {
+        $factors = [];
+
+        for ($divisor = 2; $number > 1; $divisor++){
+            for (; $number % $divisor === 0; $number /= $divisor){
+                $factors[] = $divisor;
+            }
+        }
+
+        return $factors;
+    }
+
+}
---
+++ tests/PrimeFactorsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\PrimeFactor;
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
+{
+    /**
+     * @test *
+     * @dataProvider factors
+     */
+    function it_generate_prime_factor_for_1($number, $expected){
+        $factors = new PrimeFactor;
+
+        $this->assertEquals($expected, $factors->generate($number));
+    }
+
+    public function factors()
+    {
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2,2]],
+        ];
+    }
+
+}
