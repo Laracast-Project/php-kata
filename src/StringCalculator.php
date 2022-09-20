ea27e1115a7467b3537d9946714d250aa4b7309c
Alexotieno1717 <alexotieno900@gmail.com>
2022-09-20 11:10:12 +0300

    worked on string calculator

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,7 +4,57 @@
 namespace App;
 
 
+use Exception;
+use function PHPUnit\Framework\matches;
+use function PHPUnit\Framework\throwException;
+
 class StringCalculator
 {
+    const MAX_NUMBER_ALLOWED = 1000;
+
+    protected $delimiter = ",|\n";
+
+    public function add(string $numbers){
+
+        if (! $numbers){
+            return 0;
+        }
+
+        $numbers = $this->parseString($numbers);
+
+        $numbers = $this->disallowNegative($numbers)->ignoreGreaterThan1000($numbers);
+
+        return array_sum($numbers);
+
+    }
+
+    protected function disallowNegative(array $numbers): StringCalculator
+    {
+        foreach ($numbers as $number){
+            if ($number < 0){
+                throw new Exception("Negative numbers are not allowed");
+            }
+        }
+
+        return $this;
+    }
+
+    protected function ignoreGreaterThan1000(array $numbers){
+        return array_filter($numbers, function ($number) {
+            return $number <= self::MAX_NUMBER_ALLOWED;
+        });
+    }
+
+    protected function parseString(string $numbers){
+        $customDelimiter = '\/\/(.)\n';
+
+        if (preg_match("/{$customDelimiter}/", $numbers, $matches)){
+            $this->delimiter = $matches[1];
+
+            $numbers = str_replace($matches[0], '', $numbers);
+        }
+
+        return preg_split("/{$this->delimiter}/", $numbers);
+    }
 
 }
--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -1,9 +1,67 @@
 <?php
 
 
+use App\StringCalculator;
 use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
+    /** @test */
+    function it_evaluate_an_empty_string_as_0(){
+        $calculate = new StringCalculator;
+
+        $this->assertSame(0, $calculate->add(''));
+    }
+
+    /** @test */
+    function it_find_the_sum_of_a_single_number(){
+        $calculate = new StringCalculator;
+
+        $this->assertSame(5, $calculate->add('5'));
+    }
+
+    /** @test */
+    function it_find_the_sum_of_two_number(){
+        $calculate = new StringCalculator;
+
+        $this->assertSame(10, $calculate->add('5,5'));
+    }
+
+    /** @test */
+    function it_find_the_sum_of_any_number(){
+        $calculate = new StringCalculator;
+
+        $this->assertSame(19, $calculate->add('5,5,5,4'));
+    }
+
+    /** @test */
+    function it_accept_a_new_line_character_as_delimiter_too(){
+        $calculate = new StringCalculator;
+
+        $this->assertSame(10, $calculate->add("5\n5"));
+    }
+
+    /** @test */
+    function negative_numbers_are_not_allowed(){
+        $calculate = new StringCalculator;
+
+        $this->expectException(\Exception::class);
+
+        $calculate->add("5,-4");
+    }
+
+    /** @test */
+    function numbers_greater_than_1000_are_ignored(){
+        $calculate = new StringCalculator;
+
+        $this->assertEquals(5, $calculate->add('5,1001'));
+    }
+
+    /** @test */
+    function it_suppose_custom_delimiters(){
+        $calculate = new StringCalculator;
+
+        $this->assertEquals(9, $calculate->add("//:\n5:4"));
+    }
 
 }
